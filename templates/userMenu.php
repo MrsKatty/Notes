bbeb3316dadf883c03c3b3b5a634532672148934
PavelBondarenko <59204295+PavelBondarenko@users.noreply.github.com>
2020-08-18 21:20:23 +0300

    img

--- templates/userMenu.php
+++ templates/userMenu.php
@@ -1,3 +1,6 @@
+<?php
+echo 123;
+?>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <!--    <a class="navbar-brand" href="#">Navbar</a>-->
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
@@ -5,7 +8,6 @@
         <span class="navbar-toggler-icon"></span>
     </button>
 
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
             <li class="nav-item<?= $controllerType == '' ? ' active' : '' ?>">
                 <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
@@ -25,5 +27,4 @@
             <span class="navbar-text">
                     <?= !empty($_SESSION['user']) ? $_SESSION['user']['FIO'] . '(' . $_SESSION['user']['name'] . ')' : '' ?>
                 </span>
-    </div>
 </nav>

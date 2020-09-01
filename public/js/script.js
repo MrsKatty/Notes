4c4fbd02c3b960298ce6446ddf5e1a1ec23aaef6
MrsKatty <motsnaya96@mail.ru>
2020-09-01 21:12:25 +0300

    1
    1

---
+++ public/js/script.js
@@ -0,0 +1,15 @@
+let fun = function () {
+
+    let addButton = document.getElementById("addButton");
+    addButton.innerText = addButton.innerText === "" ? "" : ""
+
+    document.getElementById("addForm").classList.toggle("hidden")
+    document.getElementById("shadow").class.toggle("hidden")
+
+}
+
+document.getElementById("addButton").onclick = fun
+
+document.getElementById("closeFormButton").onclick = fun
+
+document.getElementById("shadow").onclick = fun

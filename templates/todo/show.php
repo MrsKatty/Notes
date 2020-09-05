5754b1e7a06042a888bbc4a020e6f998997d8421
Katerina Motsnaya <motsnaya96@mail.ru>
2020-09-05 23:16:22 +0300

    date and status
    read

--- templates/todo/show.php
+++ templates/todo/show.php
@@ -27,28 +27,33 @@ echo Html::create("Pagination")
 ?>
 
 <a class="btn btn-primary" id="addButton">Добавить заявку</a>
-<form action="?action=add&type=<?= $type ?>"  id="addForm" class="hidden" method="post"
+<form action="?action=add&type=<?= $type ?>" id="addForm" class="hidden" method="post"
       class="guestbookform">
-<label> <?= $comments['project'] ?>
-    <input type="text" name="project" id="project">
-</label>
-<label> <?= $comments['content'] ?>
-    <textarea name="content" id="content" cols="30" rows="10"></textarea>
-</label>
-<label> <?= $comments['priority'] ?>
-    <input type="text" name="priority" id="priority">
-</label>
-<!--<label> --><?//= $comments['status'] ?>
-<!--    --><?//= (new Select())->setName('users_id')->setId('users_id')->setData($usersList)->html() ?>
-<!--</label>>-->
-    <label> <?= $comments['status'] ?>
-        <input type="text" name="status" id="status">
+    <label> <?= $comments['project'] ?>
+        <input type="text" name="project" id="project">
     </label>
-<label> <?= $comments['users_id'] ?>
-    <?= (new Select())->setName('users_id')->setId('users_id')->setData($usersList)->html() ?>
-</label>
-<input type="submit" value="Отправить">
-<a class="btn btn-primary" id="closeFormButton">Закрыть</a>
+    <label> <?= $comments['content'] ?>
+        <textarea name="content" id="content" cols="30" rows="10"></textarea>
+    </label>
+    <label> <?= $comments['priority'] ?>
+        <input type="text" name="priority" id="priority">
+    </label>
+    <!--<label> --><? //= $comments['status'] ?>
+    <!--    --><? //= (new Select())->setName('users_id')->setId('users_id')->setData($usersList)->html() ?>
+    <!--</label>>-->
+    <label> <?= $comments['status'] ?>
+        <?= (new Select())->setName('status')->setId('status')->setData(["0" => "Выполняющиеся", "1" => "Завершенная"])->html() ?>
+    </label>
+    <label> <?= $comments['users_id'] ?>
+        <?= (new Select())->setName('users_id')->setId('users_id')->setData($usersList)->html() ?>
+    </label>
+
+    <label> <?= $comments['dateEnd'] ?>
+        <input type="datetime-local" name="dateEnd" id="dateEnd">
+    </label>
+
+    <input type="submit" value="Отправить">
+    <a class="btn btn-primary" id="closeFormButton">Закрыть</a>
 </form>
 <div id="shadow" class="hidden"></div>
 
